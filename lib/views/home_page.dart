2b1104795bac6b40c850034a81dd44ba912ba94b
ssena-201903 <safiyesenam@gmail.com>
2025-07-31 17:51:17 +0300

    started home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -10,6 +10,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text("Merhaba Safiye!"),
+                const SizedBox(width: 10),
+                Icon(Icons.waving_hand),
+              ],
+            ),
+            Text(
+              "Bugün harika bir gün olacak!",
+              style: TextStyle(fontSize: 14, color: Colors.black38),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Wrap(
+              spacing: 12,
+              runSpacing: 12,
+              children: List.generate(
+                4,
+                (index) => AnaKartWidget(index: index),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AnaKartWidget extends StatelessWidget {
+  final int index;
+
+  const AnaKartWidget({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: (MediaQuery.of(context).size.width - 48) / 2, // 2 kart yan yana
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Center(
+        child: Text(
+          'Kart ${index + 1}',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
