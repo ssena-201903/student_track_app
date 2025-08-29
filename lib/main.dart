aa55969d656a8fb2c9b1d54257e9df50543329dc
Safiye Sena Merdin <safiyesenam@gmail.com>
2025-08-29 13:34:20 +0300

    started state management, riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:student_track/constants/constants.dart';
 import 'package:student_track/views/main_shell.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp())
+  );
 }
 
 class MyApp extends StatelessWidget {
